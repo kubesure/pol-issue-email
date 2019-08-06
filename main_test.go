2e761fdbdb6887e080b4040bff8b9836275335fd
kubesure <edakghar@gmail.com>
2019-08-06 14:01:54 +0530

    updated policy substring test case

--- main_test.go
+++ main_test.go
@@ -3,12 +3,27 @@ package main
 import (
 	"strings"
 	"testing"
+
+	e "github.com/aws/aws-lambda-go/events"
 )
 
 func TestExtractPoilcyNumber(t *testing.T) {
-	key := "unprocessed_1234567890.pdf"
-	polNumber := key[strings.Index(key, "_"):strings.Index(key, ".")]
-	if polNumber == "1234567890" {
+	key := "unprocessed/1234567890.pdf"
+	polNumber := key[strings.Index(key, "/")+1 : strings.Index(key, ".")]
+	if polNumber != "1234567890" {
 		t.Errorf("policy number not found")
+	}
+}
+
+func TestProcessEvent(t *testing.T) {
+	bucket := e.S3Bucket{Name: "kubesure-cs-1"}
+	object := e.S3Object{Key: "unprocessed/1234567890.pdf"}
+	r := e.S3EventRecord{}
+	r.S3.Bucket = bucket
+	r.S3.Object = object
+	err := processEvent(r)
+
+	if err != nil {
+		t.Errorf("S3 Event Processed %v", err)
 	}
 }
